bcee240f661ef92d8f82d6f125c6388e0d0f412c
Michael Ah Fook Mika <mmi59@cs18235kq.canterbury.ac.nz>
2019-10-07 19:14:25 +1300

    added documentation

--- players.c
+++ players.c
@@ -4,28 +4,30 @@
 void draw_enemy(Player p) 
 {
     // TODO: Make sure that the -1 and +1 doesnt go off the display
-    tinygl_draw_line(tinygl_point(0,p.prev_pos), tinygl_point(2,p.prev_pos), 0);
+    tinygl_draw_line(tinygl_point(0,p.prev_pos), tinygl_point(2,p.prev_pos), 0); 
     tinygl_draw_line(tinygl_point(1,p.prev_pos-1), tinygl_point(1,p.prev_pos+1), 0);
     tinygl_draw_point(tinygl_point(3,p.prev_pos-1), 0);
     tinygl_draw_point(tinygl_point(3,p.prev_pos+1), 0);
 
-
+    // TODO: Make sure that the player does not go out of bound, so-1 and +1 doesnt go off the display matrix
     tinygl_draw_line(tinygl_point(0,p.pos), tinygl_point(2,p.pos), 1);
     tinygl_draw_line(tinygl_point(1,p.pos-1), tinygl_point(1,p.pos+1), 1);
     tinygl_draw_point(tinygl_point(3,p.pos-1), 1);
     tinygl_draw_point(tinygl_point(3,p.pos+1), 1);
 }
-
+// to clear out the position and to make every time the light matrix is moved to another set of matrix pin.
 void draw_ally(Player player)
 {
-    tinygl_draw_point(tinygl_point(4, player.prev_pos), 0);
-    tinygl_draw_point(tinygl_point(4, player.pos), 1);
+    tinygl_draw_point(tinygl_point(4, player.prev_pos), 0);    // to clear out the previous movement of the player.
+    tinygl_draw_point(tinygl_point(4, player.pos), 1);        // to move to that current position.
 }
 
-void set_player_pos(Player *player, uint8_t pos){
-    if(pos > 1 && pos < TINYGL_HEIGHT-2) {
-        player->prev_pos = player->pos;
-        player->pos = pos; 
+
+// set the player position as it moves from left to right
+void set_player_pos(Player *player, uint8_t pos){ 
+    if(pos > 1 && pos < TINYGL_HEIGHT-2) {   
+        player->prev_pos = player->pos;                     // set the previous postion to the current position 
+        player->pos = pos;                                  // set the current position after the previous position
     }
 }
 
