56e9cd9918cc1520d65a971391392c886468f48e
Michael Ah Fook Mika <mmi59@cs18235kq.canterbury.ac.nz>
2019-10-07 19:17:26 +1300

    added documentation

--- players.h
+++ players.h
@@ -3,13 +3,14 @@
 
 #include "system.h"
 
+// A player that has the property including health, its position that is at and it previous position 
 typedef struct {
     uint8_t health;
     uint8_t pos;
     uint8_t prev_pos;
 } Player;
 
-void draw_enemy(Player p);
+void draw_enemy(Player p); 
 void draw_ally(Player player);
 
 void set_player_pos(Player *player, uint8_t position);
