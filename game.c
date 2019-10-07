9ada18afe638f6c4220c73a7d9b927c1abfca0e4
Michael Ah Fook Mika <mmi59@cs18235kq.canterbury.ac.nz>
2019-10-07 18:47:10 +1300

    Added documentation

--- game.c
+++ game.c
@@ -18,9 +18,9 @@
 
 int main(void)
 {
-    system_init();
-    pacer_init(PACER_RATE);
-    navswitch_init();
+    system_init();              // initialised the system modula
+    pacer_init(PACER_RATE);     //  initialised a rate for the time taken to excecute
+    navswitch_init();           // initial the navigation switch
     
     tinygl_init(PACER_RATE);
     tinygl_font_set(&font3x5_1);
@@ -31,25 +31,30 @@ int main(void)
 
     Player rival;
     Player ally;
-    set_player_pos(&rival, 3);
-    set_player_pos(&ally, 3);
+    set_player_pos(&rival, 3);  // set the players position in the matrix platform
+    set_player_pos(&ally, 3);   // set the players position in the matrix platform
     
 
     while(1) {
-        pacer_wait();
-        navswitch_update();        
+        pacer_wait();           // count up to until the counter is equal to the TCNT.
+        navswitch_update();     // navigation updates the North and South buttons that is declared 
+
+        // if true the North Switch is pressed
         if (navswitch_push_event_p(NAVSWITCH_NORTH)) {
-            set_player_pos(&rival, rival.pos+1);
+            set_player_pos(&rival, rival.pos+1);      // moves the player rival to the left
             //ir_uart_putc ('1');
         }
+
+        // if true the North Switch is pressed
         if (navswitch_push_event_p(NAVSWITCH_SOUTH)) {
-            set_player_pos(&rival, rival.pos-1);
+            set_player_pos(&rival, rival.pos-1);    // moves the player rival to the right
             //ir_uart_putc ('2');
         }
 
+        // Is the character to be transmitted
         if (ir_uart_read_ready_p ()) {
-            uint8_t data;
-            data = ir_uart_getc ();
+            uint8_t data; 
+            data = ir_uart_getc ();         // gets the character and stores it in data
         }
         
         draw_enemy(rival);
