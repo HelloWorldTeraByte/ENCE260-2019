ba6d6afa7b6f438f510ec27b422d5fcf427df76d
Randipa <randipa99@gmail.com>
2019-10-13 11:49:53 +1300

    Now using defines for each encoded chars

--- game.h
+++ game.h
@@ -1,11 +1,21 @@
 #ifndef GAME_H
 #define GAME_H
 
+//500,2500
+
 #define PACER_RATE 500
 #define MESSAGE_RATE 10
 
 #define BEGIN_WAIT 2500
-#define MAX_STATE_TICKS 500
+#define MAX_STATE_TICKS 200
+
+//#define ENC_IDLE 'l'
+#define ENC_BEGIN 'B'
+#define ENC_CONF 'C'
+#define ENC_MR 'd'
+#define ENC_ML 'a'
+#define ENC_RH 'z'
+#define ENC_BRH 'Z'
 
 
 typedef enum {STATE_WAIT, STATE_BEGIN, STATE_PLAY, STATE_OVER} game_state_t;
